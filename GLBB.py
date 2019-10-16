6ca95ff93d7452e9f32c8573833a3947aa0f7f05
Nur Fadillah Bustamin <48977915+FadillahBustamin@users.noreply.github.com>
2019-10-16 15:20:30 +0700

    Add files via upload
    file modul fisika GLBB(Gerak Lurus Berubah Beraturan)

---
+++ GLBB.py
@@ -0,0 +1,12 @@
+
+def kecepatanAkhirTinggi(va, a, t):
+    return va + (a*t)
+
+def kecepatanAkhirJarak(va, a, s):
+    return (va*va) + 2 * a * s
+
+def jarak(va, t, a):
+    return (va*t) + (1/2) * a * t * t
+
+def percepatan(va, vt, ta, tt):
+    return (vt-va) / (ta-tt)
